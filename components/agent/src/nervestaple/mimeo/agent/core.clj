432c697aa1df86f53b28753bd5fbe65dc8579ab6
Christopher Miles <twitch@nervestaple.com>
2026-02-10 18:10:04 -0500

    :construction: Worked on agent session handling

--- components/agent/src/nervestaple/mimeo/agent/core.clj
+++ components/agent/src/nervestaple/mimeo/agent/core.clj
@@ -1,13 +1,24 @@
 (ns nervestaple.mimeo.agent.core
   (:require
    [clojure.core.async :as async]
-   [nervestaple.mimeo.ollama.interface :as ollama]))
+   [nervestaple.mimeo.log.interface :as log]
+   [nervestaple.mimeo.ollama.interface :as ollama]
+   [nervestaple.mimeo.uuid.interface :as uuid]))
+
+(defonce ENVIRONMENTS (ref {}))
+
+(defn store-message [session request]
+  (let [environment (ENVIRONMENTS (:id session))
+        environment-out (assoc environment :messages
+                               (conj (or (environment :messages) []) request))]
+    (dosync (alter ENVIRONMENTS assoc (:id session) environment-out))
+    environment-out))
 
 (defn agent-channel-out []
   (let [model-out-chan (async/chan)]
     (async/go-loop []
       (when-let [result (async/<! model-out-chan)]
-        (println "\n" (:response result)))
+        (log/info "\n" (:response result)))
       (recur))
     model-out-chan))
 
@@ -28,4 +39,30 @@
 (defn shutdown-agent [agent-map]
   (async/close! (agent-map :request-channel))
   (async/close! (agent-map :response-channel)))
+
+(defn new-session []
+  (let [session {:id (uuid/generate-sequential)}]
+    (dosync (alter ENVIRONMENTS assoc (:id session) {}))
+    session))
+
+(defn session-channel-in [session agent-map]
+  (let [session-chan (async/chan)]
+    (async/go-loop []
+      (when-let [prompt (async/<! session-chan)]
+        (let [environment (store-message session prompt)]
+          (log/debug "Session" (session :id))
+          (async/>! (agent-map :request-channel) prompt)))
+      (recur))
+    session-chan))
+
+(defn start-session [agent-map]
+  (let [session (new-session)
+        in-chan (session-channel-in session agent-map)]
+    {:session session
+     :request-channel in-chan
+     :response-channel (agent-map :response-channel)}))
+
+(defn stop-session [session-map]
+  (async/close! (session-map :request-channel))
+  (dosync (alter ENVIRONMENTS dissoc (:id session-map))))
 
