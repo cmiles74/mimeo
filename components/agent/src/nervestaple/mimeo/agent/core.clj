f4b3caa15abffd3fb1bdbfc0e3f801d53cba02b4
Christopher Miles <twitch@nervestaple.com>
2026-02-17 17:05:11 -0500

    :construction: Made some progress on providing a conversation transcript

--- components/agent/src/nervestaple/mimeo/agent/core.clj
+++ components/agent/src/nervestaple/mimeo/agent/core.clj
@@ -1,64 +1,81 @@
 (ns nervestaple.mimeo.agent.core
   (:require
    [clojure.core.async :as async]
+   [clojure.string :as string]
    [nervestaple.mimeo.log.interface :as log]
    [nervestaple.mimeo.ollama.interface :as ollama]
    [nervestaple.mimeo.uuid.interface :as uuid]))
 
 (defonce ENVIRONMENTS (ref {}))
 
-(defn update-transcript [session type request]
+(defn register-agent [session agent-map]
+  (dosync (alter ENVIRONMENTS assoc (:id session)
+                 (merge (@ENVIRONMENTS (:id session))
+                        {:agent (dissoc agent-map :session)}))))
+
+(defn fetch-session [session-id]
+  (@ENVIRONMENTS session-id))
+
+(defn update-transcript [session type message]
   (let [environment (@ENVIRONMENTS (:id session))
         environment-out (assoc environment :messages
                                (conj (or (environment :messages) [])
-                                     {:type type :message request}))]
+                                     {:type type :message message}))]
     (dosync (alter ENVIRONMENTS assoc (:id session) environment-out))
     environment-out))
 
-(defn transcript [session]
+(defn current-transcript [session]
   (let [messages (session :messages)]
     (when messages
       (apply str
              (for [message messages]
                (str (if (= :request (message :type))
-                      "Request: " "Response: ")g
+                      "Request: " "Response: ")
                     (.trim (message :message))
                     "\n\n"))))))
 
-(defn agent-channel-out [session]
-  (let [model-out-chan (async/chan)]
-    (async/go-loop []
-      (when-let [result (async/<! model-out-chan)]
-        (update-transcript session :response (:response result))
-        (println "\n" (:response result)))
-      (recur))
-    model-out-chan))
-
-(defn agent-channel-in [connect model-name session out-chan]
+(defn agent-channel-in [connect session out-chan]
   (let [model-chan (async/chan)]
     (async/go-loop []
       (when-let [prompt (async/<! model-chan)]
-        (let [transcript (when session (transcript session))
-              environment (update-transcript session :request prompt)]
-          (async/>! out-chan
-                    (ollama/prompt connect model-name nil transcript prompt))))
+        (let [session-this (fetch-session (:id session))
+              model-name (get-in session-this [:agent :model-name])
+              system-prompt (get-in session-this [:agent :system-prompt])
+              environment (update-transcript session :request prompt)
+              transcript (when session
+                           (str "Below is a transcript of the conversation so "
+                                "far.\n\n"
+                                (current-transcript environment)))
+              result (ollama/prompt connect model-name system-prompt
+                                      (or transcript prompt))]
+          (update-transcript session :response (:response result))
+          (async/>! out-chan result)))
       (recur))
     model-chan))
-
-(defn shutdown-agent [agent-map]
-  (async/close! (agent-map :request-channel))
-  (async/close! (agent-map :response-channel)))
 
 (defn new-session []
   (let [session {:id (uuid/generate-sequential)}]
     (dosync (alter ENVIRONMENTS assoc (:id session) {}))
     session))
 
-(defn start-agent [connect model-name]
+(defn transcript [agent-map]
+  (let [session (fetch-session (get-in agent-map [:session :id]))]
+    (current-transcript session)))
+
+(defn shutdown-agent [agent-map]
+  (let [agent (fetch-session (get-in agent-map [:session :id]))]
+    (async/close! (get-in agent [:agent :request-channel]))
+    (async/close! (get-in agent [:agent :response-channel]))))
+
+(defn start-agent [connect model-name system-prompt]
   (let [session (new-session)
-        out-chan (agent-channel-out session)
-        in-chan (agent-channel-in connect model-name session out-chan)]
-    {:session session
-     :request-channel in-chan
-     :response-channel out-chan}))
+        out-chan (async/chan)
+        agent-map {:session session
+                   :model-name model-name
+                   :system-prompt system-prompt
+                   :response-channel out-chan
+                   :request-channel (agent-channel-in connect session
+                                                      out-chan)}]
+    (register-agent session agent-map)
+    (dissoc agent-map :model-out-channel :system-prompt)))
 
--- components/agent/src/nervestaple/mimeo/agent/interface.clj
+++ components/agent/src/nervestaple/mimeo/agent/interface.clj
@@ -3,13 +3,20 @@
    [nervestaple.mimeo.agent.core :as core]))
 
 (defn start-agent
-  "Accepts an active Ollama connection and a model name. Starts up a new agent and
-  returns a map with asynchronous channels for communicating with the agent."
-  [connection model-name]
-  (core/start-agent connection model-name))
+  "Accepts an active Ollama connection, a model name and a system prompt for the
+  agent. Starts up a new agent and returns a map of agent data, including
+  asynchronous channels for communicating with the agent."
+  [connection model-name system-prompt]
+  (core/start-agent connection model-name system-prompt))
 
 (defn shutdown-agent
   "Accepts a map of agent data and shuts down the agent, closing the agent's
   asynchronous channels."
   [agent-map]
   (core/shutdown-agent agent-map))
+
+(defn transcript
+  "Accepts an agent map and returns a String with the transcript of the
+  conversation."
+  [agent-map]
+  (core/transcript agent-map))
